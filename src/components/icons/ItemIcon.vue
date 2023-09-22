693aca94a5f5a073f8a2547ea2c672d99dea5c5a
unknown <uros.mrkobrada99@gmail.com>
2023-09-22 12:12:39 +0200

    added svg icons

---
+++ src/components/icons/ItemIcon.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-row">
+    <slot /> &nbsp;&nbsp;&nbsp;
+    <span> {{ props.text }} </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ text: string }>()
+</script>
---
+++ src/components/icons/Status.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 12 12">
+    <path
+      :class="{
+        'fill-red-500': props.status == 'Dead',
+        'fill-gray-500': props.status == 'unknown',
+        'fill-green-500': props.status == 'Alive'
+      }"
+      fillRule="evenodd"
+      d="M6 10a4 4 0 1 0 0-8a4 4 0 0 0 0 8zm0 2A6 6 0 1 0 6 0a6 6 0 0 0 0 12z"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ status: 'Dead' | 'Alive' | 'unknown' }>()
+</script>
